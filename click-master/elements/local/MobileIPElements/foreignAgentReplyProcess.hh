848cfdcf24c1dcc415a567f3d16f4e350a6126d7
JesseSMitsUA <JesseSmitsUA>
2017-11-26 22:03:55 +0100

    Process of reply at FA

---
+++ click-master/elements/local/MobileIPElements/foreignAgentReplyProcess.hh
@@ -0,0 +1,32 @@
+#ifndef CLICK_FOREIGNAGENTREPLYPROCESS_HH
+#define CLICK_FOREIGNAGANTREPLYPROCESS_HH
+
+#include <click/element.hh>
+#include <click/ipaddress.hh>
+#include "visitorList.hh"
+CLICK_DECLS
+
+
+class ForeignAgentReplyProcess: public Element {
+    public:
+        ForeignAgentReplyProcess();
+        ~ForeignAgentReplyProcess();
+
+        const char *class_name() const { return "ForeignAgentReplyProcess"; }
+        const char *port_count() const { return "1/1"; }
+        const char *processing() const { return PUSH; }
+
+        int configure(Vector<String>&, ErrorHandler*);
+        void push(int, Packet*);
+
+        private:
+
+            IPAddress _foreignAgent;
+            VisitorList* _visitorList;
+            short int _maxLifetime; // ??
+
+
+};
+
+CLICK_ENDDECLS
+#endif
