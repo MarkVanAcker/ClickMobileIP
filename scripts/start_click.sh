1c5b8694dbbdd55fb97802648a2bcced17fdf2e2
s0153120 <markvanacker@student.uantwerpen.be>
2017-12-29 22:01:07 +0100

    changes

--- scripts/start_click.sh
+++ scripts/start_click.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #../userlevel/click glue.click -p 10000 &
-../userlevel/click mn.click -p 10001 
+../click-master/userlevel/click mn.click -p 10001 
 #../click-master/userlevel/click ha.click -p 10002 &
 #../click-master/userlevel/click fa.click -p 10003 
 #../userlevel/click cn.click -p 10004
